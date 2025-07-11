763067e27855a8875c84eb4e742bab215e1796b3
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-07-11 15:42:14 +0200

    add fetch function
    fetch pkm
    render pkm Card

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,52 @@
-
+let pokemon = [];
 
 function init() {
-    
+    fetchPkm();
 }
+
+// main fetch
+async function fetchPkm() {
+  try {
+    let data = await fetchBaseData();
+    let detailedList = await fetchDetails(data.results);
+    renderCards(detailedList);
+  } catch (error) {
+    console.error("error loading Pokemon:", error);
+  }
+}
+
+// load Base data
+async function fetchBaseData() {
+  let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+  return await response.json();  
+}
+
+
+// load details of Pokemon
+async function fetchDetails(basicList) {
+  return await Promise.all(
+    basicList.map(async (pkm) => {
+      let res = await fetch(pkm.url);
+      let detail = await res.json();
+      return parseDetail(detail);
+    })
+  );
+}
+
+// read base data of pokemon
+function parseDetail(detail) {
+  return {
+    id: detail.id,
+    name: detail.name,
+    image: detail.sprites.front_default,
+    types: detail.types.map(t => t.type.name)
+  };
+}
+
+// render Pokemon card
+function renderCards(pokemonList) {
+  let prevCard = document.getElementById("pkmCards");
+  let html = getPkmPrevCard(pokemonList);
+  prevCard.innerHTML = html;
+}
+
--- scripts/template.js
+++ scripts/template.js
@@ -1,22 +1,34 @@
-function pkmPrevCard() {
-    return `
-    <div class="pkm_cad_prev">
-                <div id="pkmHeader" class="pkm_header">
-                    <div>
-                        <span id="pkmmId">#1</span>
-                    </div>
-                    <div>
-                        <span id="pkmName">Bisasam</span>
-                    </div>
-                </div>
-                <div class="pkm_pic">
-                    <img src="" alt="pkm_img">
-                </div>
-                <div id="pkmFooter" class="pkm_footer">
-                    <div>
-                        <img id="pkmImg" src="" alt="type_icons">
-                    </div>
-                </div>
-            </div>
-    `
+function getPkmPrevCard(pokemonList) {
+  let html = "";
+
+  pokemonList.forEach((pokemon, index) => {
+    html += `
+      <div class="pkm_cad_prev">
+        <div class="pkm_header">
+          <div>
+            <span>#${index + 1}</span>
+          </div>
+          <div>
+            <span>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
+          </div>
+        </div>
+        <div class="seperator"></div>
+        <div class="pkm_pic">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png" alt="${pokemon.name}">
+        </div>
+        <div class="seperator"></div>
+        <div class="pkm_footer">
+          <div>
+            <img src="./assets/icons/type_icons/grass.svg" alt="type_icons">
+          </div>
+          <div>
+            <img src="./assets/icons/type_icons/poison.svg" alt="">
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  return html;
 }
+
