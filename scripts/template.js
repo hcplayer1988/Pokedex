841be1ad2e3f39ca57e56a6d56d5165dc422c6d1
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-07-11 10:33:32 +0200

    update style
    sticky footer
    pkm box

---
+++ scripts/template.js
@@ -0,0 +1,22 @@
+function pkmPrevCard() {
+    return `
+    <div class="pkm_cad_prev">
+                <div id="pkmHeader" class="pkm_header">
+                    <div>
+                        <span id="pkmmId">#1</span>
+                    </div>
+                    <div>
+                        <span id="pkmName">Bisasam</span>
+                    </div>
+                </div>
+                <div class="pkm_pic">
+                    <img src="" alt="pkm_img">
+                </div>
+                <div id="pkmFooter" class="pkm_footer">
+                    <div>
+                        <img id="pkmImg" src="" alt="type_icons">
+                    </div>
+                </div>
+            </div>
+    `
+}
