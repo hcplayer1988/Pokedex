2b7f630a1ed28da0c9de36d4d33c5d5ca78e5665
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-07-14 08:14:43 +0200

    Update template.js
    template function adapted

--- scripts/template.js
+++ scripts/template.js
@@ -1,37 +1,4 @@
-// function getPkmPrevCard(pokemonList) {
-//   let html = "";
-
-//   pokemonList.forEach((pokemon, index) => {
-//     html += `
-//       <div class="pkm_cad_prev">
-//         <div class="pkm_header">
-//           <div>
-//             <span>#${index + 1}</span>
-//           </div>
-//           <div>
-//             <span>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
-//           </div>
-//         </div>
-//         <div class="seperator"></div>
-//         <div class="pkm_pic">
-//           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png" alt="${pokemon.name}">
-//         </div>
-//         <div class="seperator"></div>
-//         <div class="pkm_footer">
-//           <div>
-//             <img id="typeOne" src="./assets/icons/type_icons/grass.svg" alt="type_icons">
-//           </div>
-//           <div>
-//             <img id="typeTwo" src="./assets/icons/type_icons/poison.svg" alt="">
-//           </div>
-//         </div>
-//       </div>
-//     `;
-//   });
-
-//   return html;
-// }
-
+// Pokemon preview card template
 
 
 function getPkmPrevCard(pokemonList) {
